666b93df607c5dc64c98da3734123d0aef53f0b3
Merveille Jora <114751342+DevJora@users.noreply.github.com>
2025-10-09 17:25:52 +0200

    Init Vector2Test

---
+++ marsRover/src/test/java/org/ynov/shared/Vector2Test.java
@@ -0,0 +1,22 @@
+package org.ynov.shared;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vector2Test {
+
+    @Test
+    void fromString() {
+        Vector2 vector2 = Vector2.fromString("(-1,1)");
+        assertEquals(-1, vector2.x);
+        assertEquals(1, vector2.y);
+    }
+
+    @Test
+    void toStringTest(){
+        Vector2 vector2 = new Vector2(-1,1);
+        assertEquals(vector2.toString(),"(-1,1)");
+    }
+
+}
