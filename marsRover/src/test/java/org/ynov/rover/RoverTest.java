681b438f5b5bd82880212c8dcec548e2e2c6038b
Merveille Jora <114751342+DevJora@users.noreply.github.com>
2025-10-09 17:25:25 +0200

    Init RoverTest

---
+++ marsRover/src/test/java/org/ynov/rover/RoverTest.java
@@ -0,0 +1,22 @@
+package org.ynov.rover;
+
+import org.junit.jupiter.api.Test;
+
+public class RoverTest {
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void processInstruction() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void getOrientation() {
+    }
+}
