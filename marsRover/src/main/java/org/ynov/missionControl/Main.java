e80d00411ad2b2f0e10f4a233dd82318941def52
geolex <adrien.lexcellent@free.fr>
2025-10-10 10:37:39 +0200

    Initialized Mission Control

--- marsRover/src/main/java/org/ynov/missionControl/Main.java
+++ marsRover/src/main/java/org/ynov/missionControl/Main.java
@@ -14,5 +14,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
+        new MissionControl().Initialize();
     }
 }
