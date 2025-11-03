124e79ee1ee9b97fba49e66324fc59f3b9e1b5ef
Merveille Jora <114751342+DevJora@users.noreply.github.com>
2025-11-03 13:50:17 +0100

    draft MissionControlTest

---
+++ marsRover/src/test/java/org/ynov/missionControl/MissionControlTest.java
@@ -0,0 +1,31 @@
+package org.ynov.missionControl;
+
+class MissionControlTest {
+//    @Spy  // ou @Mock selon ton besoin
+//    private MissionControl missionControl;
+//
+//    @Test
+//    void callRealInitialize_onSpy() {
+//        doCallRealMethod().when(missionControl).Initialize();
+//        missionControl.Initialize(); // appelle la vraie méthode
+//        verify(missionControl, times(1)).Initialize();
+//    }
+//
+//
+//    @Nested
+//    class Initialize {
+//
+//
+//        @Test
+//        void testInitialize() {
+//            // GIVEN
+//
+//            // WHEN
+//
+//
+//            // THEN
+//
+//        }
+//    }
+
+}
